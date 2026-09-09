2ec765dc82a113a6840059fb4e690b90fe544699
Joshua Leong <128206550+BluePjCookies@users.noreply.github.com>
2026-09-09 02:26:58 +0000

    fix

--- utils.py
+++ utils.py
@@ -14,14 +14,3 @@ def load_json(path): # easy way to load json scripts (Satisfy file imports requi
 def get_strength(value, target, max_diff):
     return max(0, 1 - abs(value - target) / max_diff)
 
-from pydub import AudioSegment
-
-input_file = "data/audio/static.mp3"
-output_file = "data/audio/static_update.mp3"
-increase_db = 10  # Increase volume by 10 dB
-
-audio = AudioSegment.from_mp3(input_file)
-louder_audio = audio + increase_db
-louder_audio.export(output_file, format="mp3")
-
-print(f"Done! Saved as {output_file}")
